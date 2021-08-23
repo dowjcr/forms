f14c101a1a102c15d9e08d6471580ce1dfe6d6ad
Toby Boyne <tobyboyne@gmail.com>
2021-08-24 00:16:29 +0100

    Initial budget form html

--- forms/dcac/templatetags/custom_filters.py
+++ forms/dcac/templatetags/custom_filters.py
@@ -1,3 +1,6 @@
+"""Custom filters used in Django Templates
+To use in a template, add `{% load custom_filters %}` to the template"""
+
 from django import template
 from ..constants import *
 
@@ -10,4 +13,9 @@ def str_reimbursement_type(request_type):
 
 @register.filter()
 def str_fund_source(fund_source):
-    return dict(FundSources.CHOICES).get(fund_source, 'Standard')
+    return dict(FundSources.CHOICES).get(fund_source, 'Annual Consumable Grant')
+
+
+@register.filter()
+def str_budget_type(budget_type):
+    return dict(BudgetType.CHOICES).get(budget_type, 'General Funding')
