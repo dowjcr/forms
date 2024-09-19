a1f1d2c8f6de0a49cafa7c318d5fc6508687d38f
Thomas Mills <accounts+github@tomjmills.co.uk>
2024-09-19 18:37:39 +0100

    Update email recipients for budget

--- forms/budget/email.py
+++ forms/budget/email.py
@@ -14,9 +14,17 @@ from forms.constants import *
 from forms.settings import notifier
 from forms.templatetags.custom_filters import str_academic_year
 
+def budget_recipient_crsid_list(budget):
+    crsid_list = [budget.submitter]
+    presidents = budget.president_crsid.split(",")
+    crsid_list.extend(presidents)
+    treasurer = budget.treasurer_crsid.split(",")
+    crsid_list.extend(treasurer)
+    return crsid_list
+
 def notify_budget_submit(budget):
     subject = "{0} - {1} Budget Submitted".format(budget.organization.name, str_academic_year(budget.year))
-    recipient_list = [user + "@cam.ac.uk" for user in (budget.submitter, budget.president_crsid, budget.treasurer_crsid)]
+    recipient_list = [user + "@cam.ac.uk" for user in budget_recipient_crsid_list(budget)]
     html_message = render_to_string('budget/emails/budget-submitted.html', {'budget': budget})
     message = "A budget has been submitted for your society."
     notifier.SendEmail(recipients=recipient_list, subject=subject, bodyhtml=html_message, bodytext=message)
@@ -31,14 +39,14 @@ def notify_treasurer_budget(budget):
 
 def notify_budget_approved(budget):
     subject = "{0} - {1} Budget Approved".format(budget.organization.name, str_academic_year(budget.year))
-    recipient_list = [user + "@cam.ac.uk" for user in (budget.submitter, budget.president_crsid, budget.treasurer_crsid)]
+    recipient_list = [user + "@cam.ac.uk" for user in budget_recipient_crsid_list(budget)]
     html_message = render_to_string("budget/emails/budget-approved.html", {'budget': budget})
     message = "Your budget has been approved."
     notifier.SendEmail(recipients=recipient_list, subject=subject, bodyhtml=html_message, bodytext=message)
 
 def notify_budget_amounts_edited(budget):
     subject = "{0} - {1} Budget Amounts Changed".format(budget.organization.name, str_academic_year(budget.year))
-    recipient_list = [user + "@cam.ac.uk" for user in (budget.submitter, budget.president_crsid, budget.treasurer_crsid)]
+    recipient_list = [user + "@cam.ac.uk" for user in budget_recipient_crsid_list(budget)]
     html_message = render_to_string("budget/emails/budget-amounts-edited.html", {'budget': budget})
     message = "Your budget amounts have been changed."
     notifier.SendEmail(recipients=recipient_list, subject=subject, bodyhtml=html_message, bodytext=message)
